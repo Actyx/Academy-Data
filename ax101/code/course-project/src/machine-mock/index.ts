9ee8a0cff36f7b5a130e7c3d366b728f94231d41
Wolfgang Werner <mail@wolfgang-werner.net>
2021-05-04 13:10:06 +0200

    Add course project scaffolding

---
+++ ax101/code/course-project/src/machine-mock/index.ts
@@ -0,0 +1,13 @@
+import { Pond } from '@actyx/pond'
+
+Pond.default().then((pond) => {
+  // start something awesome here
+  // -------------------------------
+  //
+  // For the first time here?
+  // https://developer.actyx.com/docs/pond/getting-started
+  //
+  // You will find help and examples on:
+  // https://developer.actyx.com/docs/pond/guides/hello-world
+  // https://developer.actyx.com/blog
+})
