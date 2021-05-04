fa186cce3f1dd543ae34be76e75f1ca19373edb4
Wolfgang Werner <mail@wolfgang-werner.net>
2021-05-04 16:40:08 +0200

    Implement mock machine connector

--- ax101/code/course-project/src/machine-mock/index.ts
+++ ax101/code/course-project/src/machine-mock/index.ts
@@ -1,13 +1,50 @@
-import { Pond } from '@actyx/pond'
+// [[start:import]]
+import { Pond, Tags } from '@actyx/pond'
+// [[start:import]]
 
+// [[start:states]]
+enum MachineState {
+  IDLE,
+  RUNNING,
+  ERROR,
+}
+// [[end:states]]
+
+// [[start:change-event]]
+type MachineStateChangedEvent = {
+  eventType: 'machine_state_changed'  // fixed event type
+  device: string,                     // name of the machine that changed its state
+  state: number,                      // state code
+  stateDesc?: string,                 // state name
+}
+// [[end:change-event]]
+
+// [[start:random]]
+function randomMachineState(): MachineState {
+  const rand = Math.random()
+  if (rand < 0.2) return MachineState.IDLE
+  else if (rand < 0.9) return MachineState.RUNNING
+  else return MachineState.ERROR
+}
+// [[end:random]]
+
+// [[start:impl]]
 Pond.default().then((pond) => {
-  // start something awesome here
-  // -------------------------------
-  //
-  // For the first time here?
-  // https://developer.actyx.com/docs/pond/getting-started
-  //
-  // You will find help and examples on:
-  // https://developer.actyx.com/docs/pond/guides/hello-world
-  // https://developer.actyx.com/blog
+  setInterval(() => {
+    const newState = randomMachineState()
+    const changeEvent: MachineStateChangedEvent = {
+      eventType: 'machine_state_changed',
+      device: 'Mock Machine',
+      state: newState,
+      stateDesc: MachineState[newState]
+    }
+    console.log(`Emitting ${JSON.stringify(changeEvent)}`, pond.info())
+
+    pond.emit(
+      Tags('Machine:Mock-1', `Machine.state:${newState}`),
+      changeEvent
+    )
+
+  }, 1000)
 })
+  // [[end:impl]]
