7f99fb489bc2d4e6a0e4857830ef7400f1d0f3c1
Wolfgang Werner <mail@wolfgang-werner.net>
2021-05-10 17:48:38 +0200

    Refactor machine change event type

--- ax101/code/course-project/src/fish/events.ts
+++ ax101/code/course-project/src/fish/events.ts
@@ -1,8 +1,27 @@
 // [[start:machine-state-event]]
-export type MachineStateChangedEvent = {
-    eventType: 'machine_state_changed',  // fixed event type
-    device: string,                     // name of the machine
-    state: number,                      // state code
-    stateDesc?: string,                 // state name
-  }
+import * as t from 'io-ts'
+import { ActyxEvent } from '@actyx/pond'
 // [[end:machine-state-event]]
+// [[start:machine-state-changed-event-type-check]]
+import { isRight } from 'fp-ts/lib/Either'
+// [[end:machine-state-changed-event-type-check]]
+
+// [[start:machine-state-event]]
+export const machineStateChangedEvent = t.intersection([
+    t.type({
+        eventType: t.literal('status_changed'),
+        device: t.string,
+        state: t.number,
+    }),
+    t.partial({
+        stateDesc: t.string,
+    }),
+])
+export type MachineStateChangedEvent = t.TypeOf<typeof machineStateChangedEvent>
+// [[end:machine-state-event]]
+
+// [[start:machine-state-changed-event-type-check]]
+export const isMachineStateChangedEvent = (
+    event: ActyxEvent<unknown>,
+): event is ActyxEvent<MachineStateChangedEvent> => isRight(machineStateChangedEvent.decode(event.payload))
+// [[end:machine-state-changed-event-type-check]]
