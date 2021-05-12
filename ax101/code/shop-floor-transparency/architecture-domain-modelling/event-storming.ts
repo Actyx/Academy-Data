01ba97afdd40064ae6aa7c428c7d281b0fdf40b2
Alex Halemba <alex-halemba@gmx.de>
2021-05-12 12:07:59 +0200

    events

--- ax101/code/shop-floor-transparency/architecture-domain-modelling/event-storming.ts
+++ ax101/code/shop-floor-transparency/architecture-domain-modelling/event-storming.ts
@@ -1,7 +1,32 @@
+const uuid = { v1: () => '' }
+
 // [[start:machineStateChangedEvent]]
 type MachineStateChangedEvent = {
   eventType: 'machineStateChanged'
-  device: 'Machine1'
-  state: 'newState'
+  device: string
+  state: string
+}
+
+const stateEvent: MachineStateChangedEvent = {
+  eventType: 'machineStateChanged',
+  device: 'Machine1',
+  state: 'active',
 }
 // [[end:machineStateChangedEvent]]
+// [[start:machineErrorOccurredEvent]]
+type MachineErrorOccurredEvent = {
+  eventType: 'machineErrorOccurred'
+  device: string
+  errorId: string
+  errorCode: number
+  description: string
+}
+
+const errorEvent: MachineErrorOccurredEvent = {
+  eventType: 'machineErrorOccurred',
+  device: 'Machine1',
+  errorId: uuid.v1(),
+  errorCode: 112,
+  description: 'fire',
+}
+// [[end:machineErrorOccurredEvent]]
