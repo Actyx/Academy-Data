2d0cec47a4d4c9dcfd661167d6470dd64f5b93c5
Wolfgang Werner <mail@wolfgang-werner.net>
2021-05-28 16:03:27 +0200

    Add event type defs

--- ax101/code/course-project/src/fish/productionOrdersFish.ts
+++ ax101/code/course-project/src/fish/productionOrdersFish.ts
@@ -1,3 +1,8 @@
+// [[start:fish-skeleton]]
+// [[start:fish-skeleton]]
+import { Fish, FishId, Tag } from "@actyx/pond"
+// [[end:fish-skeleton]]
+
 // [[start:order-type]]
 type Order = {
     orderId: string
@@ -10,7 +15,38 @@ type Order = {
 // [[end:order-type]]
 
 // [[start:orders-state-type]]
-type ProductionOrdersState = {
+export type ProductionOrdersState = {
     orders: Record<string, Order>
 }
 // [[end:orders-state-type]]
+
+
+//[[start:event-type-created]]
+export type ProductionOrderCreatedEvent = {
+    eventType: 'productionOrderCreated'
+} & Order
+//[[end:event-type-created]]
+
+//[[start:event-type-started]]
+export type ProductionOrderStartedEvent = {
+    eventType: 'productionOrderStarted',
+    orderId: string
+    machineId: string
+}
+//[[end:event-type-started]]
+
+//[[start:event-type-finished]]
+export type ProductionOrderFinishedEvent = {
+    eventType: 'productionOrderFinished',
+    orderId: string
+    machineId: string
+}
+//[[end:event-type-finished]]
+
+
+// [[start:event-type-po]]
+type ProductionOrderEvent =
+    | ProductionOrderCreatedEvent
+    | ProductionOrderStartedEvent
+    | ProductionOrderFinishedEvent
+// [[end:event-type-po]]
