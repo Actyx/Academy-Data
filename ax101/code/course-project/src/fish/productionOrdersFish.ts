18a59fad87243bda0f832b2bcd851c39b7de5955
Wolfgang Werner <mail@wolfgang-werner.net>
2021-05-28 13:31:25 +0200

    Remove code from next section

--- ax101/code/course-project/src/fish/productionOrdersFish.ts
+++ ax101/code/course-project/src/fish/productionOrdersFish.ts
@@ -1,5 +1,3 @@
-import { Fish, FishId, Tag } from "@actyx/pond"
-
 // [[start:order-type]]
 type Order = {
     orderId: string
@@ -16,44 +14,3 @@ type ProductionOrdersState = {
     orders: Record<string, Order>
 }
 // [[end:orders-state-type]]
-
-// [[start:event-type]]
-type ProductionOrderEvent = undefined
-// [[end:event-type]]
-
-// [[start:tags]]
-const productionOrderTag = Tag<Event>('ProductionOrder')
-
-// [[end:tags]]
-
-// [[start:fish-skeleton]]
-export const ProductionOrdersFish = {
-    // tags
-    tags: {
-        // [[end:fish-skeleton]]
-        productionOrderTag,      
-        // [[start:fish-skeleton]]
-    },
-    // [[end:fish-skeleton]]
-
-    // [[start:fish-skeleton]]
-    // twin implementation
-    all: {
-        fishId: FishId.of('ProductionOrders', 'all', 0),
-        initialState: { orders: {} }, // initial state value of type ProductionOrdersState
-        where: productionOrderTag,
-        onEvent: (state: ProductionOrdersState, event: ProductionOrderEvent) => {
-            // [[end:fish-skeleton]]
-
-            // [[start:fish-skeleton]]
-            return state
-        }
-    } as Fish<ProductionOrdersState, ProductionOrderEvent>,
-    // [[end:fish-skeleton]]
-
-    // [[start:fish-skeleton]]
-    //emitters
-
-    // [[start:fish-skeleton]]
-}
-// [[end:fish-skeleton]]
