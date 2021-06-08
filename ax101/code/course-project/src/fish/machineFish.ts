9f228a81eaac14cfa202aec6fbd3936f2e28e7f4
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-08 12:25:30 +0200

    Add machineFish

---
+++ ax101/code/course-project/src/fish/machineFish.ts
@@ -0,0 +1,50 @@
+// [[start:import]]
+import { Fish, FishId } from '@actyx/pond'
+import {
+    ProductionOrderFinishedEvent,
+    ProductionOrdersFish,
+    ProductionOrderStartedEvent,
+} from './productionOrdersFish'
+// [[end:import]]
+
+// [[start:state]]
+export type MachineState = {
+    machineId: string
+    currentOrderId: string[]
+}
+// [[end:state]]
+
+// [[start:construction]]
+// [[start:skeleton]]
+export const MachineFish = {
+    tags: {},
+    of: (
+        machineId: string,
+    ): Fish<MachineState, ProductionOrderStartedEvent | ProductionOrderFinishedEvent> => ({
+        // [[end:construction]]
+        fishId: FishId.of('MachineState', machineId, 0),
+        initialState: { machineId, currentOrderId: [] },
+        where: ProductionOrdersFish.tags.productionOrderStartedByTag
+            .withId(machineId)
+            .or(ProductionOrdersFish.tags.productionOrderFinishedByTag.withId(machineId)),
+        onEvent: (state, event) => {
+            // [[end:skeleton]]
+            switch (event.eventType) {
+                case 'productionOrderStarted':
+                    state.currentOrderId.push(event.orderId)
+                    return state
+
+                case 'productionOrderFinished':
+                    state.currentOrderId = state.currentOrderId.filter((id) => id !== event.orderId)
+                    return state
+                default:
+                    break
+            }
+            return state
+            // [[start:skeleton]]
+        },
+        // [[start:construction]]
+    }),
+}
+// [[end:construction]]
+// [[end:skeleton]]
