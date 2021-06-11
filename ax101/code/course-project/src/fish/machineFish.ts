c7886fd51c5a2c13970f98c2e17a1ae930a71cdb
Alex Halemba <alex-halemba@gmx.de>
2021-06-11 10:37:44 +0200

    fix changed indends

--- ax101/code/course-project/src/fish/machineFish.ts
+++ ax101/code/course-project/src/fish/machineFish.ts
@@ -1,51 +1,51 @@
 // [[start:import]]
 import { Fish, FishId } from '@actyx/pond'
 import {
-  ProductionOrderFinishedEvent,
-  ProductionOrdersFish,
-  ProductionOrderStartedEvent,
+    ProductionOrderFinishedEvent,
+    ProductionOrdersFish,
+    ProductionOrderStartedEvent,
 } from './productionOrdersFish'
 // [[end:import]]
 
 // [[start:state]]
 export type MachineState = {
-  machineId: string
-  currentOrderId: string[]
+    machineId: string
+    currentOrderId: string[]
 }
 // [[end:state]]
 
 // [[start:construction]]
 // [[start:skeleton]]
 export const MachineFish = {
-  tags: {},
-  of: (
-    machineId: string,
-  ): Fish<MachineState, ProductionOrderStartedEvent | ProductionOrderFinishedEvent> => ({
-    fishId: FishId.of('MachineState', machineId, 0),
-    // [[end:construction]]
-    initialState: { machineId, currentOrderId: [] },
-    where: ProductionOrdersFish.tags.productionOrderStartedByTag
-      .withId(machineId)
-      .or(ProductionOrdersFish.tags.productionOrderFinishedByTag.withId(machineId)),
-    // [[start:on-event]]
-    onEvent: (state, event) => {
-      // [[end:skeleton]]
-      switch (event.eventType) {
-        case 'productionOrderStarted':
-          state.currentOrderId.push(event.orderId)
-          return state
-        case 'productionOrderFinished':
-          state.currentOrderId = state.currentOrderId.filter((id) => id !== event.orderId)
-          return state
-        default:
-          break
-      }
-      return state
-      // [[start:skeleton]]
-    },
-    // [[end:on-event]]
-    // [[start:construction]]
-  }),
+    tags: {},
+    of: (
+        machineId: string,
+    ): Fish<MachineState, ProductionOrderStartedEvent | ProductionOrderFinishedEvent> => ({
+        fishId: FishId.of('MachineState', machineId, 0),
+        // [[end:construction]]
+        initialState: { machineId, currentOrderId: [] },
+        where: ProductionOrdersFish.tags.productionOrderStartedByTag
+        .withId(machineId)
+        .or(ProductionOrdersFish.tags.productionOrderFinishedByTag.withId(machineId)),
+        // [[start:on-event]]
+        onEvent: (state, event) => {
+        // [[end:skeleton]]
+        switch (event.eventType) {
+            case 'productionOrderStarted':
+            state.currentOrderId.push(event.orderId)
+            return state
+            case 'productionOrderFinished':
+            state.currentOrderId = state.currentOrderId.filter((id) => id !== event.orderId)
+            return state
+            default:
+            break
+        }
+        return state
+        // [[start:skeleton]]
+        },
+        // [[end:on-event]]
+        // [[start:construction]]
+    }),
 }
 // [[end:construction]]
 // [[end:skeleton]]
