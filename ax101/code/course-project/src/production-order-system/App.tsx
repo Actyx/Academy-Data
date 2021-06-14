1e33211c12aea7bf90a443735e7f2b57059ec30c
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-14 18:03:03 +0200

    Remove app defaults

--- ax101/code/course-project/src/production-order-system/App.tsx
+++ ax101/code/course-project/src/production-order-system/App.tsx
@@ -2,25 +2,14 @@ import * as React from 'react'
 
 export const App = (): JSX.Element => {
   return (
-    <div style={{ margin: '120px auto', width: 400 }}>
-      <img src="https://raw.githubusercontent.com/actyx-contrib/react-pond/master/icon.png?token=AATHWQIC5RWS62GY3OINH3C645MHQ" />
-      <h1>Start something awesome!</h1>
-      <h4>Help and examples:</h4>
-      <ul>
-        <li>
-          <a href="https://developer.actyx.com/blog/2020/06/22/react-pond">
-            react-pond - Introduction
-          </a>
-        </li>
-        <li>
-          <a href="https://developer.actyx.com/docs/pond/getting-started">Pond - getting-started</a>
-        </li>
-        <li>
-          <a href="https://developer.actyx.com/docs/pond/guides/hello-world">
-            Guides - hello-world
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+      <h1>Production Order System</h1>
+      <div>
+        <b>TODO: Production Order Form</b>
+      </div>
+      <div>
+        <b>TODO: Production Order List</b>
+      </div>
     </div>
   )
 }
