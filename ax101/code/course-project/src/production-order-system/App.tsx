67e6afa2e542a83a0729b8f11b7b0536c0552a04
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-14 18:23:33 +0200

    Improve source tagging

--- ax101/code/course-project/src/production-order-system/App.tsx
+++ ax101/code/course-project/src/production-order-system/App.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
+import { OrderList } from './OrderList'
 
 export const App = (): JSX.Element => {
   return (
     <div className="container">
       <h1>Production Order System</h1>
       <div>
-        <b>TODO: Production Order Form</b>
+        <h2>Create Production Order</h2>
+        {/* [[end:app-skeleton]] */}
+        {/* [[start:app-skeleton]] */}
       </div>
       <div>
-        <b>TODO: Production Order List</b>
+        <h2>Production Orders</h2>
+        {/* [[end:app-skeleton]] */}
+        <OrderList />
+        {/* [[start:app-skeleton]] */}
       </div>
     </div>
   )
 }
+// [[end:app-skeleton]]
