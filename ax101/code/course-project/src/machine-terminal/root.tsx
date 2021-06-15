6e22f85ae6546f42bc13b4dc127912a17189bd3f
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-15 16:16:49 +0200

    Scaffold machine terminal app

---
+++ ax101/code/course-project/src/machine-terminal/root.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Pond } from '@actyx-contrib/react-pond'
+import { App } from './App'
+
+// add bootstrap CSS
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const onError = () => {
+  setTimeout(() => location.reload(), 2500)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Pond loadComponent={<div>Connecting to ActyxOS</div>} onError={onError}>
+      <App />
+    </Pond>
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
