f6c23fe99f911b59c543df9b3099407fd631e891
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-15 12:12:03 +0200

    Add source tags for binding

--- ax101/code/course-project/src/production-order-system/OrderList.tsx
+++ ax101/code/course-project/src/production-order-system/OrderList.tsx
@@ -1,14 +1,18 @@
 // [[start:skeleton]]
+// [[start:use-fish]]
 import * as React from 'react'
 // [[end:skeleton]]
 import { useFish } from '@actyx-contrib/react-pond'
 import { ProductionOrdersFish } from '../fish/productionOrdersFish'
+// [[end:use-fish]]
 
+// [[start:use-fish]]
 // [[start:skeleton]]
 export const OrderList = (): JSX.Element => {
   // [[end:skeleton]]
   const productionOrdersFish = useFish(ProductionOrdersFish.all)
   const orders = Object.values(productionOrdersFish.state.orders)
+  // [[end:use-fish]]
 
   // [[start:skeleton]]
   return (
@@ -20,6 +24,7 @@ export const OrderList = (): JSX.Element => {
           <th scope="col">Product</th>
         </tr>
       </thead>
+      {/* [[start:bind-data]]  */}
       <tbody>
         {/* [[end:skeleton]] */}
         {orders.map((order) => (
@@ -33,7 +38,10 @@ export const OrderList = (): JSX.Element => {
         ))}
         {/* [[start:skeleton]]  */}
       </tbody>
+      {/* [[end:bind-data]]  */}
     </table>
   )
+  // [[start:use-fish]]
 }
+// [[end:use-fish]]
 // [[end:skeleton]]
