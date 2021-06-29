aed02ef66c088d17f29fd26e04c024049f7fdad4
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-29 13:16:21 +0200

    Source tagging

--- ax101/code/course-project/src/machine-terminal/MaterialBatch.tsx
+++ ax101/code/course-project/src/machine-terminal/MaterialBatch.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
+// [[start:add-batch-info]]
 import { useFishFn } from '@actyx-contrib/react-pond'
 import { MaterialBatchFishes } from '../fish/materialBatchFish'
+// [[end:add-batch-info]]
 
 // [[start:material-batch-component]]
 type MaterialBatchProps = {
@@ -9,64 +11,47 @@ type MaterialBatchProps = {
 
 export const MaterialBatch = ({ batchId }: MaterialBatchProps): JSX.Element => {
   // [[end:material-batch-component]]
+
+  // [[start:add-batch-info]]
   const batchFish = useFishFn(MaterialBatchFishes.of, batchId)
 
   if (!batchFish || batchFish.state.state === 'unknown') {
     return <div>loading...</div>
   }
   const state = batchFish.state
+  // [[end:add-batch-info]]
 
+  // [[start:add-batch-per-order]]
   const byOrder = Object.entries(batchFish.state.consumedByOrder)
-  /*
-  homework:
-  const byMachine = Object.entries(state.consumedByMachine)
-  */
+  // [[end:add-batch-per-order]]
 
-// [[start:material-batch-component]]
-return (
-  <table className="table">
-        <tbody>
-         {/* [[end:material-batch-component]] */} 
-          <tr>
-            <td scope="row">batch number</td>
-            <td>{state.batchId}</td>
+  // [[start:material-batch-component]]
+  return (
+    <table className="table">
+      <tbody>
+        {/* [[end:material-batch-component]] */}
+        {/* [[start:add-batch-info]] */}
+        <tr>
+          <td scope="row">batch number</td>
+          <td>{state.batchId}</td>
+        </tr>
+        {/* [[end:add-batch-info]] */}
+        {/* [[start:add-batch-per-order]] */}
+        <tr>
+          <th scope="col" colSpan={2}>
+            Consumed material by Production Order
+          </th>
+        </tr>
+        {byOrder.map(([order, amount]) => (
+          <tr key={order}>
+            <td scope="row">{order}</td>
+            <td>{amount}</td>
           </tr>
-          {/*
-          homework:
-          <tr>
-            <td scope="row">batch size</td>
-            <td>{state.batchSize}</td>
-          </tr>
-          <tr>
-            <td scope="row">available Material</td>
-            <td>{state.availableMaterial}</td>
-          </tr>
-          */}
-          <tr>
-            <th scope="col" colSpan={2}>
-              Consumed material by Production Order
-            </th>
-          </tr>
-          {byOrder.map(([order, amount]) => (
-            <tr key={order}>
-              <td scope="row">{order}</td>
-              <td>{amount}</td>
-            </tr>
-          ))}
-          {/* Homework: <tr>
-            <th scope="col" colSpan={2}>
-              Consumed material by Machine
-            </th>
-          </tr>
-          {byMachine.map(([machine, amount]) => (
-            <tr key={machine}>
-              <td scope="row">{machine}</td>
-              <td>{amount}</td>
-            </tr>
-          ))} */}
-        {/* [[start:material-batch-component]] */} 
-        </tbody>
-      </table>
+        ))}
+        {/* [[end:add-batch-per-order]] */}
+        {/* [[start:material-batch-component]] */}
+      </tbody>
+    </table>
   )
 }
 // [[end:material-batch-component]]
