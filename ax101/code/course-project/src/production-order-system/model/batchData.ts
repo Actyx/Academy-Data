2805587c99156c1868dacd7c202f93ed60b93a69
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-29 19:01:37 +0200

    Add model

---
+++ ax101/code/course-project/src/production-order-system/model/batchData.ts
@@ -0,0 +1,44 @@
+// [[start:registry]]
+import { useRegistryFish } from '@actyx-contrib/react-pond'
+import { MaterialBatchFishes, DefinedState } from '../../fish/materialBatchFish'
+// [[end:registry]]
+
+// [[start:model]]
+export type BatchData = Array<{
+  batchId: string
+  batchSize: number
+  availableMaterial: number
+  machines: string[]
+  orders: string[]
+}>
+// [[end:model]]
+
+// [[start:model]]
+// [[start:registry]]
+// [[start:map]]
+export const batchData = (): BatchData => {
+  // [[end:map]]
+  // [[end:model]]
+  const materialBatches = useRegistryFish(
+    MaterialBatchFishes.all,
+    Object.keys,
+    MaterialBatchFishes.of,
+    )
+    // [[end:registry]]
+    
+    // [[start:map]]
+    return materialBatches
+    .map((batch) => batch.state)
+    .filter((batch): batch is DefinedState => batch.state === 'defined')
+    .map(({ batchId, batchSize, availableMaterial, consumedByMachine, consumedByOrder }) => ({
+      batchId,
+      batchSize,
+      availableMaterial,
+      machines: Object.keys(consumedByMachine),
+      orders: Object.keys(consumedByOrder),
+    }))
+    // [[start:model]]
+    // [[start:registry]]
+  }
+  // [[end:registry]]
+  // [[end:model]]
