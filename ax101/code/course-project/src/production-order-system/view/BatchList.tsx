58e09ca86cb334ae363346aa678fb06a097b9112
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-30 12:28:00 +0200

    Add view

---
+++ ax101/code/course-project/src/production-order-system/view/BatchList.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { BatchData } from '../model/batchData'
+
+type BatchListComponentProps = {
+  allBatches: BatchData
+  onFreeze: (batchId: string) => void
+}
+export const BatchList = ({ allBatches, onFreeze }: BatchListComponentProps): JSX.Element => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Batch Number</th>
+          <th scope="col">Size</th>
+          <th scope="col">available material</th>
+          <th scope="col">used by</th>
+          <th scope="col">used for order</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {allBatches.map(({ batchId, batchSize, availableMaterial, machines, orders }) => (
+          <tr key={batchId}>
+            <th scope="row">{batchId.substr(0, 13)}</th>
+            <td>{batchSize}</td>
+            <td>{availableMaterial}</td>
+            <td>{machines.join(', ')}</td>
+            <td>{orders.join(', ')}</td>
+            <td>
+              <button onClick={() => onFreeze(batchId)}>freeze</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
