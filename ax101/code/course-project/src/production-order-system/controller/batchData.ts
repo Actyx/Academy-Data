a897c6f8e6219883c2b0d7ae5c87efab9ae7e1ed
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-30 12:43:24 +0200

    Add controller

---
+++ ax101/code/course-project/src/production-order-system/controller/batchData.ts
@@ -0,0 +1,13 @@
+export const freezeBatch = (batchId: string) => {
+  alert(`Implementation missing:\n${batchId} is not going to be frozen.`)
+
+  // Practice exercise:
+  // Implement a freeze methode into the Batch-Material-Twin
+
+  //   1. add frozen event
+  //   2. add emitter
+  //   3. add frozen State
+  //   4. add new behaviour to onEvent Function
+  //   5. add new behaviour to machine-connector
+  //   6. update worker-ui to show frozen notification
+}
