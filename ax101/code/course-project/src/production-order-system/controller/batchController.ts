a4b6c4483908d07226596f1211cf148fc3e19b08
Wolfgang Werner <mail@wolfgang-werner.net>
2021-06-30 12:46:10 +0200

    Rename batch controller, improve comment

--- ax101/code/course-project/src/production-order-system/controller/batchController.ts
+++ ax101/code/course-project/src/production-order-system/controller/batchController.ts
@@ -2,7 +2,10 @@ export const freezeBatch = (batchId: string) => {
   alert(`Implementation missing:\n${batchId} is not going to be frozen.`)
 
   // Practice exercise:
-  // Implement a freeze methode into the Batch-Material-Twin
+  // Implement a freeze methode into the Batch-Material-Twin. 
+  // Operators may freeze a batch if they encounter quality problems
+  // regarding the raw materials in the batch. 
+  // Materials from a 'frozen'  batch may not be consumed. 
 
   //   1. add frozen event
   //   2. add emitter
