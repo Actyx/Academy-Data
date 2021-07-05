cf2646fdf3107918b13b470b94c9632588ae27fb
Alex Halemba <alex-halemba@gmx.de>
2021-07-05 12:39:03 +0200

    missing change

--- ax101/code/course-project/src/input-material-scanner-connector/manifest.ts
+++ ax101/code/course-project/src/input-material-scanner-connector/manifest.ts
@@ -2,7 +2,7 @@ import { AppManifest } from "@actyx/pond"
 
 export const manifest: AppManifest = {
   appId: 'com.example.node.app',
-  displayName: 'machine-mock',
+  displayName: 'input-material-scanner-connector',
   version: '0.0.1'
 }
 
